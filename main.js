d6239e385fff26b01de9b26ea2249c1a34c0707e
csubiii <csubicsabi05@gmail.com>
2023-01-29 12:11:21 +0100

    🌎  Sphere created

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
 import './style.css'
 import * as THREE from "three";
+
+//Scene
+const Scene = new THREE.Scene();
+
+//Sphere
+const geometry = new THREE.SphereGeometry(3, 64, 64)
+const material = new THREE.MeshStandardMaterial({
+  color: "#0080ff",
+  roughness: 0.1
+})
